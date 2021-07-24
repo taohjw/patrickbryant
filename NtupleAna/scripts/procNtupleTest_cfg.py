d7c25694b63ffdf4aa1b73539c2b6bd0853479ce
taohjw <zhousunaomex@126.com>
2021-07-25 01:18:28 +0800

    more command line features

--- NtupleAna/scripts/procNtupleTest_cfg.py
+++ NtupleAna/scripts/procNtupleTest_cfg.py
@@ -10,6 +10,10 @@ parser = optparse.OptionParser()
 parser.add_option('-d', '--debug',                dest="debug",         action="store_true", default=False, help="debug")
 parser.add_option('-m', '--isMC',                 dest="isMC",          action="store_true", default=False, help="isMC")
 parser.add_option('-y', '--year',                 dest="year",          default="2016", help="Year specifies trigger (and lumiMask for data)")
+parser.add_option('-l', '--lumi',                 dest="lumi",          default="1",    help="Luminosity for MC normalization: units [pb]")
+parser.add_option(      '--bTagger',              dest="bTagger",       default="CSVv2", help="bTagging algorithm")
+parser.add_option('-b', '--bTag',                 dest="bTag",          default="0.8484", help="bTag cut value: default is medium WP for CSVv2 https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation80XReReco")
+parser.add_option('-i', '--input',                dest="input",         default="ZZ4b/fileLists/data2016H.txt", help="Input file(s). If it ends in .txt, will treat it as a list of input files.")
 parser.add_option('-o', '--outputBase',           dest="outputBase",    default="/uscms/home/bryantp/nobackup/ZZ4b/", help="Base path for storing output histograms and picoAOD")
 parser.add_option('-p', '--createPicoAOD',        dest="createPicoAOD", action="store_true", default=False, help="Create picoAOD from original NANOAOD even if picoAOD already exists")
 o, a = parser.parse_args()
@@ -18,26 +22,30 @@ o, a = parser.parse_args()
 # Basic Configuration
 #
 debug      = o.debug
-outputBase = "/uscms/home/bryantp/nobackup/ZZ4b/"
+outputBase = o.outputBase
 inputFile, isMC  = "ZZ4b/fileLists/data2016H.txt", False
 #inputFile, isMC  = "ZZ4b/fileLists/ZH_bbqq.txt", True
 #inputFile, isMC  = "ZZ4b/fileLists/ZZ_bbbb.txt", True
-bTag, bTagger    = 0.8484, "CSVv2" #medium WP for CSVv2 https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation80XReReco
+bTag, bTagger    = float(o.bTag), o.bTagger
 isData     = not isMC
 blind      = True and isData
 year       = o.year
 JSONfile   = 'ZZ4b/lumiMasks/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
-lumi       = 150e3
+lumi       = float(o.lumi)
+
+fileNames = []
+if ".txt" in o.input:
+    for line in open(inputFile, 'r').readlines():
+        line = line.replace('\n','').strip()
+        if line    == '' : continue
+        if line[0] == '#': continue
+        fileNames.append(line.replace('\n',''))
+else:
+    fileNames.append(o.input)
+fileNames = cms.vstring(fileNames)
+
 
 process = cms.PSet()
-fileNames = []
-for line in open(inputFile, 'r').readlines():
-    line = line.replace('\n','').strip()
-    if line    == '' : continue
-    if line[0] == '#': continue
-    fileNames.append(line.replace('\n',''))
-
-fileNames = cms.vstring(fileNames)
 
 #Setup framework lite input file object
 process.fwliteInput = cms.PSet(
