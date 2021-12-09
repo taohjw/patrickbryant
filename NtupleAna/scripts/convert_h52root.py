48cac1d856c4b37da61c3e515a7a2f7d1b1ce9db
taohjw <zhousunaomex@126.com>
2021-12-09 13:50:05 +0800

    fix convert by using clonetree instead of deleting branches

--- NtupleAna/scripts/convert_h52root.py
+++ NtupleAna/scripts/convert_h52root.py
@@ -21,44 +21,44 @@ print df.iloc[0]
 f = ROOT.TFile(args.outfile, "UPDATE")
 tree = f.Get("Events;1")
 
+def addOrUpdate(branch):
+    if branch in df:
+        if "nil" in str(tree.FindBranch(branch)):
+            print "Add", branch
+        else:
+            print "Update", branch
+            tree.SetBranchStatus(branch, 0)
+            # b = tree.GetBranch("ZHvsBackgroundClassifier")
+            # tree.GetListOfBranches().Remove(b)
+            # l = tree.GetLeaf("ZHvsBackgroundClassifier")
+            # tree.GetListOfLeaves().Remove(l)
+        return True
+    return False
+        
+class variable:
+    def __init__(self, name):
+        self.name = name
+        self.convert = addOrUpdate(name)
+        self.array = np.float32([0])
 
-ZHvsBackgroundClassifierStatus = False
-if "ZHvsBackgroundClassifier" in df:
-    ZHvsBackgroundClassifierStatus = True
-    if "nil" in str(tree.FindBranch("ZHvsBackgroundClassifier")):
-        print "Add ZHvsBackgroundClassifier"
-    else:
-        print "Update ZHvsBackgroundClassifier"
-        #tree.SetBranchStatus("ZHvsBackgroundClassifier",0)
-        b = tree.GetBranch("ZHvsBackgroundClassifier")
-        tree.GetListOfBranches().Remove(b)
-        l = tree.GetLeaf("ZHvsBackgroundClassifier")
-        tree.GetListOfLeaves().Remove(l)
+variables = [variable("ZHvsBackgroundClassifier"),
+             variable("nTagClassifier")]
 
-
-nTagClassifierStatus = False
-if "nTagClassifier" in df:
-    nTagClassifierStatus = True
-    if "nil" in str(tree.FindBranch("nTagClassifier")):
-        print "Add nTagClassifier"
-    else:
-        print "Update nTagClassifier"
-        #tree.SetBranchStatus("nTagClassifier", 0)
-        b = tree.GetBranch("nTagClassifier")
-        tree.GetListOfBranches().Remove(b)
-        l = tree.GetLeaf("nTagClassifier")
-        tree.GetListOfLeaves().Remove(l)
-
-
-f.Write(tree.GetName(), ROOT.gROOT.kOverwrite)
-f.Close()
-print tree
-
-if nTagClassifierStatus == False and ZHvsBackgroundClassifierStatus == False: 
+overwrite=False
+for variable in variables:
+    if variable.convert: overwrite=True
+if not overwrite:
     print "Nothing to add or update..."
     exit()
 
+print "Clone tree"
+newTree = tree.CloneTree()
+print "Overwrite tree"
+f.Write(tree.GetName(), ROOT.gROOT.kOverwrite)
+print "Close",args.outfile
+f.Close()
 
+print "Reopen",args.outfile
 f = ROOT.TFile(args.outfile, "UPDATE")
 f.ls()
 newTree = f.Get("Events;1")
@@ -66,17 +66,20 @@ print newTree
 #newTree.Show(0)
 
 
-nTagClassifier = np.float32([0])
-ZHvsBackgroundClassifier = np.float32([0])
-if           nTagClassifierStatus: newTree.Branch(          "nTagClassifier",          nTagClassifier,            "nTagClassifier/F")
-if ZHvsBackgroundClassifierStatus: newTree.Branch("ZHvsBackgroundClassifier", ZHvsBackgroundClassifier, "ZHvsBackgroundClassifier/F")
+convertVariables=[]
+for variable in variables:
+    if variable.convert:
+        convertVariables.append(variable)
+        newTree.Branch(variable.name, variable.array, variable.name+"/F")
+variables=convertVariables
 
 n=0
 for i, row in df.iterrows():
     n+=1
     newTree.GetEntry(i)
-    if           nTagClassifierStatus:           nTagClassifier[0] = row[          "nTagClassifier"]
-    if ZHvsBackgroundClassifierStatus: ZHvsBackgroundClassifier[0] = row["ZHvsBackgroundClassifier"]
+    for variable in variables:
+        if variable.convert:
+            variable.array[0] = row[variable.name]
     newTree.Fill()
 
     if(i+1)%10000 == 0 or (i+1) == df.shape[0]:
