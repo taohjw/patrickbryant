685931fdb9eac583ace6f3164599c75260faae28
taohjw <zhousunaomex@126.com>
2025-09-03 07:09:35 +0800

    fix path parsing

--- nTupleAnalysis/scripts/convert_root2h5.py
+++ nTupleAnalysis/scripts/convert_root2h5.py
@@ -2,7 +2,7 @@ import pandas as pd
 import ROOT
 ROOT.gROOT.SetBatch(True)
 import numpy as np
-import os, sys
+import os, sys, time
 from glob import glob
 from copy import copy
 import multiprocessing
@@ -56,7 +56,7 @@ variables = [variable("FvT"),
              variable("xW"),
              #variable("xWt"),
              #variable("xt"),
-             variable("m4j"),
+             # variable("m4j"),
              variable("SB", dtype=np.bool_), variable("CR", dtype=np.bool_), variable("SR", dtype=np.bool_),
              #variable("ZZSB", dtype=np.bool_), variable("ZZCR", dtype=np.bool_), variable("ZZSR", dtype=np.bool_),             
              #variable("ZHSB", dtype=np.bool_), variable("ZHCR", dtype=np.bool_), variable("ZHSR", dtype=np.bool_),             
@@ -69,11 +69,11 @@ variables = [variable("FvT"),
              variable("dRjjClose"), variable("dRjjOther"),
              variable("nSelJets", dtype=np.uint32),
              variable("nPSTJets", dtype=np.uint32),
-             variable("st"),
-             variable("stNotCan"),
-             variable("aveAbsEta"),
-             variable("aveAbsEtaOth"),
-             variable("nPVsGood"),
+             # variable("st"),
+             # variable("stNotCan"),
+             # variable("aveAbsEta"),
+             # variable("aveAbsEtaOth"),
+             # variable("nPVsGood"),
              variable("d01TruthMatch", dtype=np.int32),
              variable("d23TruthMatch", dtype=np.int32),
              variable("d02TruthMatch", dtype=np.int32),
@@ -138,10 +138,10 @@ def convert(inFile):
     print " >> nEvts:",nEvts
     outFile = args.outFile if args.outFile else inFile.replace(".root",".h5")
     outFile = outFile.split('/')
-    outDir, outFile  = ''.join(outFile[:-1]), outFile[-1]
+    outDir, outFile  = '/'.join(outFile[:-1])+'/', outFile[-1]    
     store=None
     xrdcpOutFile = False
-    if "root://" in outFile:
+    if "root://" in outDir:
         xrdcpOutFile = True
         print " >> make file locally and then xrdcp to final destination"
         print " >> remote directory:",outDir
@@ -162,8 +162,9 @@ def convert(inFile):
     assert iEvtEnd <= nEvts
     print " >> Processing entries: [",iEvtStart,"->",iEvtEnd,")"
 
-    sw = ROOT.TStopwatch()
-    sw.Start()
+    # sw = ROOT.TStopwatch()
+    # sw.Start()
+    start = time.time()
 
     nWritten = 0
     for iEvtStart in range(0,iEvtEnd,chunkSize):
@@ -172,12 +173,12 @@ def convert(inFile):
                 'canJet0_phi': [], 'canJet1_phi': [], 'canJet2_phi': [], 'canJet3_phi': [],
                 'canJet0_m'  : [], 'canJet1_m'  : [], 'canJet2_m'  : [], 'canJet3_m'  : [],
                 #'canJet0_e'  : [], 'canJet1_e'  : [], 'canJet2_e'  : [], 'canJet3_e'  : [],
-                'm01': [], 'm23': [], 'm02': [], 'm13': [], 'm03': [], 'm12': [], 
-                'm123': [], 'm023': [], 'm013': [], 'm012': [],
-                'pt01': [], 'pt23': [], 'pt02': [], 'pt13': [], 'pt03': [], 'pt12': [], 
-                'dR01': [], 'dR23': [], 'dR02': [], 'dR13': [], 'dR03': [], 'dR12': [], 
-                's4j': [],
-                'dR0123': [], 'dR0213': [], 'dR0312': [],
+                # 'm01': [], 'm23': [], 'm02': [], 'm13': [], 'm03': [], 'm12': [], 
+                # 'm123': [], 'm023': [], 'm013': [], 'm012': [],
+                # 'pt01': [], 'pt23': [], 'pt02': [], 'pt13': [], 'pt03': [], 'pt12': [], 
+                # 'dR01': [], 'dR23': [], 'dR02': [], 'dR13': [], 'dR03': [], 'dR12': [], 
+                # 's4j': [],
+                # 'dR0123': [], 'dR0213': [], 'dR0312': [],
                 } 
         nOthJetsMax = 12
         for i in range(nOthJetsMax):
@@ -195,7 +196,11 @@ def convert(inFile):
             # Initialize event
             tree.GetEntry(iEvt)
             if (iEvt+1) % 1000 == 0 or iEvt+1 == iEvtEnd:
-                sys.stdout.write("\rProcessed %10d of %10d"%(iEvt+1,nEvts)+" | "+str(int((iEvt+1)*100.0/nEvts))+"% ")
+                elapsed = time.time()-start
+                rate = (iEvt+1)/elapsed
+                secondsRemaining = (nEvts-iEvt)/rate
+                h, m, s = int(secondsRemaining/3600), int(secondsRemaining/60)%60, int(secondsRemaining)%60
+                sys.stdout.write("\rProcessed %10d of %10d (%4.0f/s) | %3.0f%% (done in %02d:%02d:%02d)"%(iEvt+1,nEvts,rate, (iEvt+1)*100.0/nEvts, h,m,s))
                 sys.stdout.flush()
 
 
@@ -206,10 +211,10 @@ def convert(inFile):
             data['canJet0_phi'].append(copy(tree.canJet0_phi)); data['canJet1_phi'].append(copy(tree.canJet1_phi)); data['canJet2_phi'].append(copy(tree.canJet2_phi)); data['canJet3_phi'].append(copy(tree.canJet3_phi))
             #if canJet_m_Status:
             data['canJet0_m'].append(copy(tree.canJet0_m)); data['canJet1_m'].append(copy(tree.canJet1_m)); data['canJet2_m'].append(copy(tree.canJet2_m)); data['canJet3_m'].append(copy(tree.canJet3_m))
-            jets[0].SetPtEtaPhiM(tree.canJet0_pt, tree.canJet0_eta, tree.canJet0_phi, tree.canJet0_m)
-            jets[1].SetPtEtaPhiM(tree.canJet1_pt, tree.canJet1_eta, tree.canJet1_phi, tree.canJet1_m)
-            jets[2].SetPtEtaPhiM(tree.canJet2_pt, tree.canJet2_eta, tree.canJet2_phi, tree.canJet2_m)
-            jets[3].SetPtEtaPhiM(tree.canJet3_pt, tree.canJet3_eta, tree.canJet3_phi, tree.canJet3_m)
+            # jets[0].SetPtEtaPhiM(tree.canJet0_pt, tree.canJet0_eta, tree.canJet0_phi, tree.canJet0_m)
+            # jets[1].SetPtEtaPhiM(tree.canJet1_pt, tree.canJet1_eta, tree.canJet1_phi, tree.canJet1_m)
+            # jets[2].SetPtEtaPhiM(tree.canJet2_pt, tree.canJet2_eta, tree.canJet2_phi, tree.canJet2_m)
+            # jets[3].SetPtEtaPhiM(tree.canJet3_pt, tree.canJet3_eta, tree.canJet3_phi, tree.canJet3_m)
 
             # else:
             #     # data['canJet0_e'].append(copy(tree.canJet0_e)); data['canJet1_e'].append(copy(tree.canJet1_e)); data['canJet2_e'].append(copy(tree.canJet2_e)); data['canJet3_e'].append(copy(tree.canJet3_e))
@@ -221,60 +226,60 @@ def convert(inFile):
 
 
 
-            d01, d23 = jets[0]+jets[1], jets[2]+jets[3]
-            d02, d13 = jets[0]+jets[2], jets[1]+jets[3]
-            d03, d12 = jets[0]+jets[3], jets[1]+jets[2]
+            # d01, d23 = jets[0]+jets[1], jets[2]+jets[3]
+            # d02, d13 = jets[0]+jets[2], jets[1]+jets[3]
+            # d03, d12 = jets[0]+jets[3], jets[1]+jets[2]
 
-            m01, m23 = d01.M(), d23.M()
-            m02, m13 = d02.M(), d13.M()
-            m03, m12 = d03.M(), d12.M()
-            data['m01'].append(m01)
-            data['m23'].append(m23)
-            data['m02'].append(m02)
-            data['m13'].append(m13)
-            data['m03'].append(m03)
-            data['m12'].append(m12)
+            # m01, m23 = d01.M(), d23.M()
+            # m02, m13 = d02.M(), d13.M()
+            # m03, m12 = d03.M(), d12.M()
+            # data['m01'].append(m01)
+            # data['m23'].append(m23)
+            # data['m02'].append(m02)
+            # data['m13'].append(m13)
+            # data['m03'].append(m03)
+            # data['m12'].append(m12)
 
-            m123 = (jets[1]+jets[2]+jets[3]).M() #missing 0
-            m023 = (jets[0]+jets[2]+jets[3]).M() #missing 1
-            m013 = (jets[0]+jets[1]+jets[3]).M() #missing 2
-            m012 = (jets[0]+jets[1]+jets[2]).M() #missing 3
-            data['m123'].append(m123)
-            data['m023'].append(m023)
-            data['m013'].append(m013)
-            data['m012'].append(m012)
+            # m123 = (jets[1]+jets[2]+jets[3]).M() #missing 0
+            # m023 = (jets[0]+jets[2]+jets[3]).M() #missing 1
+            # m013 = (jets[0]+jets[1]+jets[3]).M() #missing 2
+            # m012 = (jets[0]+jets[1]+jets[2]).M() #missing 3
+            # data['m123'].append(m123)
+            # data['m023'].append(m023)
+            # data['m013'].append(m013)
+            # data['m012'].append(m012)
 
-            pt01, pt23 = d01.Pt(), d23.Pt()
-            pt02, pt13 = d02.Pt(), d13.Pt()
-            pt03, pt12 = d03.Pt(), d12.Pt()
-            data['pt01'].append(pt01)
-            data['pt23'].append(pt23)
-            data['pt02'].append(pt02)
-            data['pt13'].append(pt13)
-            data['pt03'].append(pt03)
-            data['pt12'].append(pt12)
+            # pt01, pt23 = d01.Pt(), d23.Pt()
+            # pt02, pt13 = d02.Pt(), d13.Pt()
+            # pt03, pt12 = d03.Pt(), d12.Pt()
+            # data['pt01'].append(pt01)
+            # data['pt23'].append(pt23)
+            # data['pt02'].append(pt02)
+            # data['pt13'].append(pt13)
+            # data['pt03'].append(pt03)
+            # data['pt12'].append(pt12)
 
-            dR01 = jets[0].DeltaR(jets[1])
-            dR23 = jets[2].DeltaR(jets[3])
-            dR02 = jets[0].DeltaR(jets[2])
-            dR13 = jets[1].DeltaR(jets[3])
-            dR03 = jets[0].DeltaR(jets[3])
-            dR12 = jets[1].DeltaR(jets[2])
-            data['dR01'].append(dR01)
-            data['dR23'].append(dR23)
-            data['dR02'].append(dR02)
-            data['dR13'].append(dR13)
-            data['dR03'].append(dR03)
-            data['dR12'].append(dR12)
+            # dR01 = jets[0].DeltaR(jets[1])
+            # dR23 = jets[2].DeltaR(jets[3])
+            # dR02 = jets[0].DeltaR(jets[2])
+            # dR13 = jets[1].DeltaR(jets[3])
+            # dR03 = jets[0].DeltaR(jets[3])
+            # dR12 = jets[1].DeltaR(jets[2])
+            # data['dR01'].append(dR01)
+            # data['dR23'].append(dR23)
+            # data['dR02'].append(dR02)
+            # data['dR13'].append(dR13)
+            # data['dR03'].append(dR03)
+            # data['dR12'].append(dR12)
 
-            dR0123 = d01.DeltaR(d23)
-            dR0213 = d02.DeltaR(d13)
-            dR0312 = d03.DeltaR(d12)
-            data['dR0123'].append(dR0123)
-            data['dR0213'].append(dR0213)
-            data['dR0312'].append(dR0312)
+            # dR0123 = d01.DeltaR(d23)
+            # dR0213 = d02.DeltaR(d13)
+            # dR0312 = d03.DeltaR(d12)
+            # data['dR0123'].append(dR0123)
+            # data['dR0213'].append(dR0213)
+            # data['dR0312'].append(dR0312)
 
-            data['s4j'].append(tree.canJet0_pt + tree.canJet1_pt + tree.canJet2_pt + tree.canJet3_pt)
+            # data['s4j'].append(tree.canJet0_pt + tree.canJet1_pt + tree.canJet2_pt + tree.canJet3_pt)
 
             for i in range(nOthJetsMax):
                 if i < tree.nAllNotCanJets:
@@ -298,36 +303,36 @@ def convert(inFile):
 
         #print
 
-        data['s4j'] = np.array(data['s4j'], np.float32)
+        # data['s4j'] = np.array(data['s4j'], np.float32)
         data['canJet0_pt'] = np.array(data['canJet0_pt'], np.float32); data['canJet1_pt'] = np.array(data['canJet1_pt'], np.float32); data['canJet2_pt'] = np.array(data['canJet2_pt'], np.float32); data['canJet3_pt'] = np.array(data['canJet3_pt'], np.float32)
         data['canJet0_eta'] = np.array(data['canJet0_eta'], np.float32); data['canJet1_eta'] = np.array(data['canJet1_eta'], np.float32); data['canJet2_eta'] = np.array(data['canJet2_eta'], np.float32); data['canJet3_eta'] = np.array(data['canJet3_eta'], np.float32)
         data['canJet0_phi'] = np.array(data['canJet0_phi'], np.float32); data['canJet1_phi'] = np.array(data['canJet1_phi'], np.float32); data['canJet2_phi'] = np.array(data['canJet2_phi'], np.float32); data['canJet3_phi'] = np.array(data['canJet3_phi'], np.float32)
         data['canJet0_m'] = np.array(data['canJet0_m'], np.float32); data['canJet1_m'] = np.array(data['canJet1_m'], np.float32); data['canJet2_m'] = np.array(data['canJet2_m'], np.float32); data['canJet3_m'] = np.array(data['canJet3_m'], np.float32)
-        data['m01'] = np.array(data['m01'], np.float32)
-        data['m23'] = np.array(data['m23'], np.float32)
-        data['m02'] = np.array(data['m02'], np.float32)
-        data['m13'] = np.array(data['m13'], np.float32)
-        data['m03'] = np.array(data['m03'], np.float32)
-        data['m12'] = np.array(data['m12'], np.float32)
-        data['m123'] = np.array(data['m123'], np.float32)
-        data['m023'] = np.array(data['m023'], np.float32)
-        data['m013'] = np.array(data['m013'], np.float32)
-        data['m012'] = np.array(data['m012'], np.float32)
-        data['pt01'] = np.array(data['pt01'], np.float32)
-        data['pt23'] = np.array(data['pt23'], np.float32)
-        data['pt02'] = np.array(data['pt02'], np.float32)
-        data['pt13'] = np.array(data['pt13'], np.float32)
-        data['pt03'] = np.array(data['pt03'], np.float32)
-        data['pt12'] = np.array(data['pt12'], np.float32)
-        data['dR01'] = np.array(data['dR01'], np.float32)
-        data['dR23'] = np.array(data['dR23'], np.float32)
-        data['dR02'] = np.array(data['dR02'], np.float32)
-        data['dR13'] = np.array(data['dR13'], np.float32)
-        data['dR03'] = np.array(data['dR03'], np.float32)
-        data['dR12'] = np.array(data['dR12'], np.float32)
-        data['dR0123'] = np.array(data['dR0123'], np.float32)
-        data['dR0213'] = np.array(data['dR0213'], np.float32)
-        data['dR0312'] = np.array(data['dR0312'], np.float32)
+        # data['m01'] = np.array(data['m01'], np.float32)
+        # data['m23'] = np.array(data['m23'], np.float32)
+        # data['m02'] = np.array(data['m02'], np.float32)
+        # data['m13'] = np.array(data['m13'], np.float32)
+        # data['m03'] = np.array(data['m03'], np.float32)
+        # data['m12'] = np.array(data['m12'], np.float32)
+        # data['m123'] = np.array(data['m123'], np.float32)
+        # data['m023'] = np.array(data['m023'], np.float32)
+        # data['m013'] = np.array(data['m013'], np.float32)
+        # data['m012'] = np.array(data['m012'], np.float32)
+        # data['pt01'] = np.array(data['pt01'], np.float32)
+        # data['pt23'] = np.array(data['pt23'], np.float32)
+        # data['pt02'] = np.array(data['pt02'], np.float32)
+        # data['pt13'] = np.array(data['pt13'], np.float32)
+        # data['pt03'] = np.array(data['pt03'], np.float32)
+        # data['pt12'] = np.array(data['pt12'], np.float32)
+        # data['dR01'] = np.array(data['dR01'], np.float32)
+        # data['dR23'] = np.array(data['dR23'], np.float32)
+        # data['dR02'] = np.array(data['dR02'], np.float32)
+        # data['dR13'] = np.array(data['dR13'], np.float32)
+        # data['dR03'] = np.array(data['dR03'], np.float32)
+        # data['dR12'] = np.array(data['dR12'], np.float32)
+        # data['dR0123'] = np.array(data['dR0123'], np.float32)
+        # data['dR0213'] = np.array(data['dR0213'], np.float32)
+        # data['dR0312'] = np.array(data['dR0312'], np.float32)
 
         for i in range(nOthJetsMax):
             data['notCanJet'+str(i)+'_pt'] = np.array(data['notCanJet'+str(i)+'_pt'], np.float32)
@@ -345,18 +350,23 @@ def convert(inFile):
         del data
 
     store.close()
-    sw.Stop()
+    elapsed = time.time()-start
+    rate = nWritten/elapsed
+    h, m, s = int(elapsed/3600), int(elapsed/60)%60, int(elapsed)%60
+
     print
-    print " >> nWritten:",nWritten
-    print " >> Real time:",sw.RealTime()/60.,"minutes"
-    print " >> CPU time: ",sw.CpuTime() /60.,"minutes"
+    print " >> Wrote %10d events in %02d:%02d:%02d (%4.0f/s)"%(nWritten, h,m,s, rate)
+    # print " >> Real time:",sw.RealTime()/60.,"minutes"
+    # print " >> CPU time: ",sw.CpuTime() /60.,"minutes"
     print " >> ======================================"
     
     if xrdcpOutFile:
         cmd = 'xrdcp -f %s %s%s'%(outFile, outDir, outFile)
         print cmd
         os.system(cmd)
-
+        cmd = 'rm '+outFile
+        print cmd
+        os.system(cmd)
 
 
 workers = multiprocessing.Pool(min(len(inFiles),3))
@@ -369,4 +379,4 @@ for output in workers.imap_unordered(convert,inFiles):
         print "finished converting",done,"of",len(inFiles),"files"
 
 for f in inFiles: print "converted:",f
-print "done"
+#print "done"
